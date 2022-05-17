ab8f9145156e26806da8869398b9b8788f647409
25wwells <105305194+25wwells@users.noreply.github.com>
2022-05-17 09:08:25 -0700

    Update Drone 2.py

--- Drone 2.py
+++ Drone 2.py
@@ -1,1 +1,34 @@
+from djitellopy import tello
+from time import sleep
 
+Mi = tello.Tello()
+Mi.connect()
+print(Mi.get_battery())
+
+Mi.takeoff()
+
+# Takeoff Point
+Mi.move_up(95)
+Mi.move_forward(325)
+sleep(.5)
+Mi.move_forward(325)
+sleep(1)
+
+# Moving in to the must fly zone
+Mi.rotate_counter_clockwise(90)
+Mi.move_forward(325)
+sleep(1)
+
+# Going to the start I think
+Mi.rotate_counter_clockwise(90)
+Mi.move_forward(325)
+sleep(.5)
+Mi.move_forward(325)
+sleep(1)
+
+# Landing zone, probably, maybe
+Mi.rotate_counter_clockwise(90)
+Mi.move_forward(325)
+sleep(1)
+
+Mi.land()
